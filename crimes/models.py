0e2eeb9a5f4fbae6a7372956a6ecd1c30ede4296
HaeGwon <gwon99065@naver.com>
2024-05-14 01:59:05 +0900

    basic code build

---
+++ crimes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Crime(models.Model):
+    date = models.DateTimeField()
+    category = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.category
