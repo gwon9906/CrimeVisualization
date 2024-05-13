5fb3e787f9dacb583bce538318d337928de52052
HaeGwon <gwon99065@naver.com>
2024-05-14 01:59:05 +0900

    basic code build

---
+++ crimes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Crime
+from collections import Counter
+
+def crime_chart(request):
+    data = Crime.objects.all()
+    categories = Counter([crime.category for crime in data])
+    categories = sorted(categories.items())
+    return render(request, 'crimes/chart.html', {'categories': categories})
