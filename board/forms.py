c35df81786484af4b8bb9f62e5e84232c8c0b2a1
SeungJin051 <seungjin051@gmail.com>
2024-05-21 02:33:36 +0900

    Update forms.py

--- board/forms.py
+++ board/forms.py
@@ -8,3 +8,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'border-2 border-gray-300 p-2 w-full'}),
+        }
