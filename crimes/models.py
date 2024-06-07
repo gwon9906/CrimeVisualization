35e1ded81d37a87acf9e69e646c6a59136484eb3
gwon <gwon99065@naver.com>
2024-06-08 03:18:58 +0900

    Crime data set

--- crimes/models.py
+++ crimes/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from django.utils import timezone
-
-
-# Create your models here.
 
 class CrimeData(models.Model):
-    crime_id = models.AutoField(primary_key=True)
-    crime_type = models.CharField(max_length=255)
-    date = models.DateField()
-    location = models.CharField(max_length=255)
-    frequency = models.IntegerField()
+    station = models.CharField(max_length=100)
+    crime_type = models.CharField(max_length=100)
+    year = models.IntegerField()
+    murder = models.IntegerField()
+    robbery = models.IntegerField()
+    sexual_crime = models.IntegerField()
+    theft = models.IntegerField()
+    violence = models.IntegerField()
 
     def __str__(self):
-        return f"{self.crime_type} at {self.location} on {self.date}"
-
+        return f"{self.station} - {self.crime_type} - {self.year}"
--- crimes/views.py
+++ crimes/views.py
@@ -1,35 +1,33 @@
-# crimes/views.py
-
 from django.shortcuts import render
-from .models import CrimeData
+from crime_map.models import CrimeData  # crime_map의 모델을 가져옴
 
 class CrimeDataSystem:
     @staticmethod
     def search_crime_data(region):
-        return CrimeData.objects.filter(location__icontains=region)
+        return CrimeData.objects.filter(station__icontains=region)
 
     @staticmethod
-    def add_crime_data(crime_type, date, location, frequency):
+    def add_crime_data(station, crime_type, year, count):
         return CrimeData.objects.create(
+            station=station,
             crime_type=crime_type,
-            date=date,
-            location=location,
-            frequency=frequency
+            year=year,
+            count=count,
         )
 
-def search_crime_data_view(request): # 검색 뷰
-    if request.method == 'POST':    # POST 요청이 들어왔을 때
-        region = request.POST.get('region') # region을 POST로 받아옴
+def search_crime_data_view(request):  # 검색 뷰
+    if request.method == 'POST':  # POST 요청이 들어왔을 때
+        region = request.POST.get('region')  # region을 POST로 받아옴
         crime_data = CrimeDataSystem.search_crime_data(region)  # CrimeDataSystem의 search_crime_data 메서드를 호출
-        return render(request, 'crimes/search_results.html', {'crime_data': crime_data})    # 검색 결과를 search_results.html에 전달
-    return render(request, 'crimes/search.html')    # GET 요청이 들어왔을 때는 search.html을 렌더링
+        return render(request, 'crimes/search_results.html', {'crime_data': crime_data})  # 검색 결과를 search_results.html에 전달
+    return render(request, 'crimes/search.html')  # GET 요청이 들어왔을 때는 search.html을 렌더링
 
 def add_crime_data_view(request):
     if request.method == 'POST':
+        station = request.POST.get('station')
         crime_type = request.POST.get('crime_type')
-        date = request.POST.get('date')
-        location = request.POST.get('location')
-        frequency = request.POST.get('frequency')
-        CrimeDataSystem.add_crime_data(crime_type, date, location, frequency)
+        year = request.POST.get('year')
+        count = request.POST.get('count')
+        CrimeDataSystem.add_crime_data(station, crime_type, year, count)
         return render(request, 'crimes/add_success.html')
     return render(request, 'crimes/add_crime_data.html')
