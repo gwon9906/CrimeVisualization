860a1efca52152d45a047d0283ea0cb3a10883f5
손봉균 <124877603+sbg0629@users.noreply.github.com>
2024-06-10 22:07:21 +0900

    디비연결 현황듸우기

--- crime_map/urls.py
+++ crime_map/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import index, crime_data, crime_details, get_crime_data
-from .views import index, crime_data, crime_details
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,4 +7,6 @@ urlpatterns = [
     path('crime_data/<str:crime_type>/<int:year>/<str:station>/', crime_details, name='crime_details'),
     path('details/', crime_details, name='crime_details'),
     path('get_crime_data/<str:crime_type>/', get_crime_data, name='get_crime_data'),
+    path('crime_data/<str:crime_type>/<int:year>/<str:station>/', crime_data, name='crime_data'),
+     path('crime_data/<str:crime_type>/<int:year>/<str:station>/', crime_data, name='crime_data_with_filters'),
 ]
